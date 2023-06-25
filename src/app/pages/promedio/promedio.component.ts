a934ae15c6014b5f312a81613c125db8855bffa8
RichardCode1997 <ericksson12021997@gmail.comgit>
2023-06-25 16:12:04 -0500

    Pregunta2_Richard

--- src/app/pages/promedio/promedio.component.ts
+++ src/app/pages/promedio/promedio.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./promedio.component.css']
 })
 export class PromedioComponent {
+  resultado: string = '';
+  calificacion1: number | null = null;
+  calificacion2: number | null = null;
+  calificacion3: number | null = null;
 
+  calcularPromedio() {
+    if (this.calificacion1 !== null && this.calificacion2 !== null && this.calificacion3 !== null) {
+      const promedio = (this.calificacion1 + this.calificacion2 + this.calificacion3) / 3;
+      this.resultado = 'El promedio es: ' + promedio.toFixed(2);
+    } else {
+      this.resultado = 'Por favor, ingrese todas las calificaciones.';
+    }
+  }
 }
